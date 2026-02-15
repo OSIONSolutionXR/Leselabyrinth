b338f79eed674a97f69e20cdaabaae9f17f072a6
OSION <www.marcelseidler@googlemail.com>
2026-02-15 12:55:44 +0100

    Update boss.js

--- src/game/boss.js
+++ src/game/boss.js
@@ -1,1 +1,27 @@
+import { clamp, vibrate } from "../core/utils.js";
+
+export function renderBossUI(el, node, sceneState) {
+  if (node.boss) {
+    el.bossHud.classList.add("show");
+    el.bossName.textContent = node.boss.name;
+    const hpMax = node.boss.hpMax;
+    const hp = clamp(sceneState.bossHP, 0, hpMax);
+    el.bossHPText.textContent = hp;
+    el.bossBarFill.style.width = ((hp / hpMax) * 100) + "%";
+  } else {
+    el.bossHud.classList.remove("show");
+  }
+}
+
+export function bossHitFX(el) {
+  el.bossHud.classList.add("hit", "bossShake");
+  el.dmg.textContent = `-1`;
+  el.dmg.classList.remove("show");
+  void el.dmg.offsetWidth;
+  el.dmg.classList.add("show");
+  vibrate(50);
+
+  setTimeout(() => el.bossHud.classList.remove("hit"), 380);
+  setTimeout(() => el.bossHud.classList.remove("bossShake"), 260);
+}
 
